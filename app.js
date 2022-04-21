fd30363186605b6bab7f352dbf6ca57818244382
lAbYriNth <rxnayeemimi@gmail.com>
2022-04-21 10:45:53 +0600

    trying to implement private messaging

--- app.js
+++ app.js
@@ -6,34 +6,45 @@ const { Server } = require("socket.io");
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        // origin: "http://127.0.0.1:8080" // Point: The allowed origin
-        origin: "*"
-    }
+	cors: {
+		// origin: "http://127.0.0.1:8080" // Point: The allowed origin
+		origin: "*"
+	}
 });
+
+const connected = [];
 
 // Segment: Socket
 // Part: Sending Global Events
 function sendTime() {
-    io.emit("time", { time: new Date().toJSON() });
+	io.emit("time", {
+		time: new Date().toJSON(),
+		users: connected
+	});
 }
-setInterval(sendTime, 100000);
+setInterval(sendTime, 10000);
 
 // Part: Listening Global "connection" Event and Responsing to Client
 io.on("connection", function (socket) {
-    // Key: Sending Local Events to Client
-    socket.emit("welcome", { message: "Welcome!", id: socket.id });
+	connected.push(socket.id);
 
-    // Part: Listening for Local Events from Client
-    socket.on("new client", console.log); // Note: console.log is a callback function/method here that will be called by socket with the received parameter!
+	// Key: Sending Local Events to Client
+	socket.emit("welcome", {
+		message: "Welcome!",
+		id: socket.id
+	});
 
-    // Key: Listening Local Events
-    socket.on("send", (name, msg) => {
-        // Key: Sending Global Events to All Clients
-        io.emit("sent", `${name}: ${msg}`);
-        // Key: Sending Local Event to a Specific Client
-        // socket.emit("deleted", "Deleted Successfully");
-    });
+	// Part: Listening for Local Events from Client
+	socket.on("new client", console.log); // Note: console.log is a callback function/method here that will be called by socket with the received parameter!
+
+	// Key: Listening Local Events
+	socket.on("send", (name, msg) => {
+		// Key: Sending Global Events to All Clients
+		// io.to()
+		io.emit("sent", `${name}: ${msg}`);
+		// Key: Sending Local Event to a Specific Client
+		// socket.emit("deleted", "Deleted Successfully");
+	});
 });
 
 httpServer.listen(3001);
--- script.js
+++ script.js
@@ -10,41 +10,42 @@ const nameEl = document.getElementById("name");
 
 // Segment: DOM Operations
 function sendMessage(message) {
-    const text = document.createTextNode(message);
-    const el = document.createElement("li");
+	const text = document.createTextNode(message);
+	const el = document.createElement("li");
 
-    el.appendChild(text);
-    messages.appendChild(el);
+	el.appendChild(text);
+	messages.appendChild(el);
 }
 
 // Segment: Socket
 // Key: Listening for Events from Server
 socket.on("welcome", function (data) {
-    sendMessage(data.message);
+	sendMessage(data.message);
 
-    // Key: Sending Response to Server
-    socket.emit("new client", data.id);
+	// Key: Sending Response to Server
+	socket.emit("new client", data.id);
 });
 
 // Key: Listening for Events from Server
 socket.on("time", function (data) {
-    sendMessage(data.time);
+	sendMessage(data.time);
+	console.log(data.users);
 });
 
 // Key: Initiating Delete Request
 if (btn) {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
+	btn.addEventListener("click", (e) => {
+		e.preventDefault();
 
-        const name = nameEl.value;
-        const msg = btn.previousElementSibling.value;
+		const name = nameEl.value;
+		const msg = btn.previousElementSibling.value;
 
-        socket.emit("send", name, msg);
-    });
+		socket.emit("send", name, msg);
+	});
 }
 
 socket.on("sent", (msg) => {
-    sendMessage(msg);
+	sendMessage(msg);
 });
 
 socket.on("error", console.error.bind(console));
